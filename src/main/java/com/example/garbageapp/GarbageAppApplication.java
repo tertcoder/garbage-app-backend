b0a8aab0669353daeed15ec00e6d36ef2b8ddb36
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2025-05-22 10:34:03 +0200

    initialize Spring Boot project with dependencies

---
+++ src/main/java/com/example/garbageapp/GarbageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.garbageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/garbageapp/GarbageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garbageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
