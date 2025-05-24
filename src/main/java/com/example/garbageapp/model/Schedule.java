f178b9e83b984d5b3050b6c072c1dfc04d04a718
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2025-05-24 10:30:18 +0200

    feat: create Schedule model with schedule types

---
+++ src/main/java/com/example/garbageapp/model/Schedule.java
@@ -0,0 +1,38 @@
+package com.example.garbageapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "schedules")
+public class Schedule {
+    
+    @Id
+    private String id;
+    
+    private String areaId;
+    
+    private LocalDateTime pickupDate;
+    
+    private ScheduleType type;
+    
+    @CreatedDate
+    private LocalDateTime createdAt;
+    
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    
+    public enum ScheduleType {
+        REGULAR,
+        SPECIAL
+    }
+}
